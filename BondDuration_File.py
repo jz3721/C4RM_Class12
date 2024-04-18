0100a7a91fedf38f9c4ee160e8c46afff6033b14
jz3721 <166566827+jz3721@users.noreply.github.com>
2024-04-18 16:40:06 -0400

    final commit

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -8,6 +8,7 @@ def getBondDuration(y, face, couponRate, m, ppy=1):
     discount_factors = (1 + y / ppy) ** periods
     present_values = cash_flows / discount_factors
     weighted_times = periods * present_values
+    
     bond_duration = np.sum(weighted_times) / np.sum(present_values)
     
     return bond_duration
